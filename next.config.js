7e7c67a8c4c762756a1065a9a4c0dea385d5bbb5
macchiitaka <zxcvbnmtacashi@gmail.com>
2021-01-31 16:47:10 +0900

    perf(loading): use preact

--- next.config.js
+++ next.config.js
@@ -5,4 +5,14 @@ module.exports = {
   },
   compress: true,
   poweredByHeader: false,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      react: 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react-dom': 'preact/compat',
+    }
+
+    return config
+  },
 }
