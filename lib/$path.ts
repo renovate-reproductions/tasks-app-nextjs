ed4f32e3291b9dc044d915a4abc17c48868463e6
macchiitaka <zxcvbnmtacashi@gmail.com>
2021-02-20 01:11:37 +0900

    style(prettier): ignore /lib/$path.ts

--- lib/$path.ts
+++ lib/$path.ts
@@ -1,15 +1,12 @@
 /* eslint-disable */
 export const pagesPath = {
-  $url: (url?: { hash?: string }) => ({
-    pathname: '/' as const,
-    hash: url?.hash,
-  }),
+  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
 }
 
 export type PagesPath = typeof pagesPath
 
 export const staticPath = {
-  favicon_ico: '/favicon.ico',
+  favicon_ico: '/favicon.ico'
 } as const
 
 export type StaticPath = typeof staticPath
