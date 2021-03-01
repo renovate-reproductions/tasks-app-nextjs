07eb1298a6cef23cdd1faa3540845c4772e29894
macchiitaka <zxcvbnmtacashi@gmail.com>
2021-03-01 16:07:32 +0900

    chore(analytics): support Matomo

--- src/infrastructure/controllers/_app.tsx
+++ src/infrastructure/controllers/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { ThemeProvider } from 'styled-components'
@@ -14,15 +15,26 @@ const queryClient = new QueryClient({
   },
 })
 
-export const MyApp = ({ Component, pageProps }: AppProps) => (
-  <QueryClientProvider client={queryClient}>
-    {process.env.NODE_ENV !== 'production' && (
-      <ReactQueryDevtools initialIsOpen={false} />
-    )}
+export const MyApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    ;(async () => {
+      ;(await import('@socialgouv/matomo-next')).init({
+        url: process.env.NEXT_PUBLIC_MATOMO_URL ?? '',
+        siteId: process.env.NEXT_PUBLIC_MATOMO_SITE_ID ?? '',
+      })
+    })()
+  }, [])
 
-    <GlobalStyle />
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  </QueryClientProvider>
-)
+  return (
+    <QueryClientProvider client={queryClient}>
+      {process.env.NODE_ENV !== 'production' && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
+
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </QueryClientProvider>
+  )
+}
