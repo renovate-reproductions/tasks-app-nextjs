164904853e7c65b9aac5464116831fe88c6257af
macchiitaka <zxcvbnmtacashi@gmail.com>
2021-04-06 17:02:02 +0900

    chore(pathpida): ignore prettier

--- lib/$path.ts
+++ lib/$path.ts
@@ -3,10 +3,13 @@ export const pagesPath = {
   $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
 }
 
+// prettier-ignore
 export type PagesPath = typeof pagesPath
 
+// prettier-ignore
 export const staticPath = {
   favicon_ico: '/favicon.ico'
 } as const
 
+// prettier-ignore
 export type StaticPath = typeof staticPath
