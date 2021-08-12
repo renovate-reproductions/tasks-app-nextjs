b5431752257e2139e815caaa0fdc473da2ceb41f
Takashi Machinaga <zxcvbnmtacashi@gmail.com>
2021-08-12 16:52:02 +0900

    chore(next.js): use esm

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next/dist/next-server/server/config-shared').NextConfig} */
 const config = {
+  experimental: {
+    esmExternals: true,
+  },
   typescript: {
     ignoreDevErrors: true,
     ignoreBuildErrors: true,
