463749de4805aa6a31760edda00303dc442178cd
Takashi Machinaga <zxcvbnmtacashi@gmail.com>
2021-10-02 23:26:53 +0900

    chore(babel): fix configuration

--- .babelrc.js
+++ .babelrc.js
@@ -2,12 +2,9 @@
 const config = {
   presets: ['next/babel'],
   plugins: [
-    [
-      '@babel/plugin-proposal-private-property-in-object',
-      {
-        loose: true,
-      },
-    ],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
     [
       'styled-components',
       {
