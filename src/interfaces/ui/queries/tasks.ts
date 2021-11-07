f036d8f6edc421a1e0a94d7e6ff9427d9336abbe
Takashi Machinaga <zxcvbnmtacashi@gmail.com>
2021-11-08 00:12:01 +0900

    refactor: optimize react query

--- src/interfaces/ui/queries/tasks.ts
+++ src/interfaces/ui/queries/tasks.ts
@@ -8,13 +8,14 @@ import { api } from '../../api';
 import { TaskRepository } from '../../repositories/task-repository';
 
 export const taskKeys = {
-  all: () => ['tasks'] as const,
+  all: () => [{ scope: 'tasks' }] as const,
   // lists: () => [...taskKeys.all(), 'list'] as const,
   // list: () => [...taskKeys.lists()] as const,
-  list: () => [...taskKeys.all(), 'list'] as const,
+  list: () => [{ ...taskKeys.all()[0], entity: 'list' }] as const,
   // details: () => [...taskKeys.all(), 'detail'] as const,
   // detail: (id: number) => [...taskKeys.details(), id] as const,
-  detail: (id: number) => [...taskKeys.all(), 'detail', id] as const,
+  detail: (id: number) =>
+    [{ ...taskKeys.all()[0], entity: 'detail', id }] as const,
 };
 
 export const fetchTasks = async (
