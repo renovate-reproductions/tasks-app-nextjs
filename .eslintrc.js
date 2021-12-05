439b27a37c2026d2ddb3bcdd691fad923108e4f6
Takashi Machinaga <zxcvbnmtacashi@gmail.com>
2021-12-05 23:53:20 +0900

    test(jest): setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,12 +6,28 @@ module.exports = {
     'next/core-web-vitals',
     'plugin:react/jsx-runtime',
     'plugin:compat/recommended',
-    'plugin:jest-dom/recommended',
-    'plugin:testing-library/dom',
-    'plugin:testing-library/react',
-    'plugin:storybook/recommended',
   ],
   parserOptions: {
     project: './tsconfig.json',
   },
+  overrides: [
+    {
+      files: ['*.@(test|spec).@(ts|tsx|js|jsx|mjs|cjs)'],
+      extends: [
+        'plugin:jest/recommended',
+        'plugin:jest-dom/recommended',
+        'plugin:testing-library/dom',
+        'plugin:testing-library/react',
+      ],
+      settings: {
+        jest: {
+          version: 27,
+        },
+      },
+    },
+    {
+      files: ['*.stories.@(ts|tsx|js|jsx|mjs|cjs)'],
+      extends: ['plugin:storybook/recommended'],
+    },
+  ],
 };
